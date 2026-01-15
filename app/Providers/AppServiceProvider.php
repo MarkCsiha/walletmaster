17b8a32d68e8e76e0d289e5566bcde7791120e49
MarkCsiha <markcsiha46@outlook.com>
2026-01-15 12:21:00 +0100

    Email hitelesítés stílus testreszabása - folyamatban.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Auth\Notifications\VerifyEmail;
+use Illuminate\Notifications\Messages\MailMessage;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,5 +22,17 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
+
+
+    VerifyEmail::toMailUsing(function (object $notifiable, string $url) {
+        return (new MailMessage)
+            ->subject('Regisztráció megerősítése')
+            ->greeting("Kedves ".$notifiable->vez_nev." ".$notifiable->ker_nev."!")
+            ->line("Köszönjük, hogy regisztrált a WalletMaster weboldalára.")
+            ->line('Kérjük, erősítse meg e-mail címét az alábbi gombra kattintva: ')
+            ->action('E-mail cím megerősítése', $url)
+            ->line("Ha nem Ön regisztrált weboldalunkon, kérjük hagyja figyelmen kívül ezt az üzenetet.")
+            ->greeting("Üdvözlettel: A WalletMaster csapata");
+    });
     }
 }
