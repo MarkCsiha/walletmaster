d69532a85e7f9b41cdea40c63cc9ca906fe8a276
MarkCsiha <markcsiha46@outlook.com>
2026-01-29 20:54:55 +0100

    web.php konfliktus megoldódott

--- routes/web.php
+++ routes/web.php
@@ -79,8 +79,8 @@ Route::get("/main", [WMController::class, "Main"])->name('naptar');
 Route::get("/add", [WMController::class, "Add"]);
 Route::post("/add", [WMController::class, "AddBtn"]);
 
-Route::get('/debts/{id}/decision', [DebtController::class, 'ShowDebtDetails'])->name('debts.decision');
+Route::get('/debts/{id}/decision', [DebtController::class, 'ShowDebtDetails'])->name('debts.decision')->middleware(["auth", "verified"]);
 
-Route::post("/debts/{id}/accept", [DebtController::class, "AcceptDebt"])->name('debts.accept');;
+Route::post("/debts/{id}/accept", [DebtController::class, "AcceptDebt"])->name('debts.accept');
 
 Route::post("/debts/{id}/reject", [DebtController::class, "RejectDebt"])->middleware("auth");
