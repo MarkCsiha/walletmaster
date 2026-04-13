59ecc734471774a88ff0f89a9445789dea6be743
MarkCsiha <markcsiha46@outlook.com>
2026-04-13 20:28:47 +0200

    Teszt javítás

--- tests/Feature/FixTest.php
+++ tests/Feature/FixTest.php
@@ -5,6 +5,8 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Models\User;
+use App\Models\szamla;
 
 class FixTest extends TestCase
 {
